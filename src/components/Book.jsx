47d09096a55e4111b78027898d7659b754b57f6b
Luke Nzang'u <saintlukke@gmail.com>
2022-06-02 17:41:00 +0300

    Add previous and next functionality

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -9,32 +9,42 @@ function Book(){
     const [currentPage, setCurrentPage] = useState(0)
     const [leftPage, setLeftPage] = useState(null)
     const [rightPage, setRightPage] = useState(null)
+    const [prevDisabled, setPrevDisabled] = useState(false)
+    const [nextDisabled, setNextDisabled] = useState(false)
     
     useEffect(()=>{
-        console.log(book);
         if (book) {
             setLeftPage(book.pages[currentPage])
             setRightPage(book.pages[currentPage+1])
+            if (currentPage+2 >= book.pages.length) {
+                setNextDisabled(true)
+            }else{
+                setNextDisabled(false)
+            }
+            if (currentPage-2 < 0) {
+                setPrevDisabled(true)
+            }else{
+                setPrevDisabled(false)
+            }
         }
-    },[currentPage])
+    },[book, currentPage])
     
     // const totalPages = book.pages.length
     const handleNext = () =>{
-        setCurrentPage(currentPage+1)
+        setCurrentPage(currentPage+2)
+        
     }
     const handlePrevious = () =>{
-        setCurrentPage(currentPage-1)
+        setCurrentPage(currentPage-2)
     }
     
     return (<div id="Book">
-        <button onClick={handlePrevious}>Previous</button>
-        <button onClick={handleNext}>Next</button>
-        {
-            book && (<div>
-                <div><Page page={leftPage} /></div>
-                <div><Page page={rightPage} /></div>
-            </div>)
-        }
+        <button onClick={handlePrevious} disabled={prevDisabled}>Previous</button>
+        <button onClick={handleNext} disabled={nextDisabled}>Next</button>
+        <div>
+            <div><Page page={leftPage} /></div>
+            <div><Page page={rightPage} /></div>
+        </div>
     </div>)
 }
 
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,32 +2,36 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Page(props){
+    
     const transformContent = (page) => {
         if (page) {
-            console.log('Transforming page', page);
-            let newContent = []
-            newContent.push('\"')
-            let currentIndex = 0
-            page.tokens.forEach((token, indx) => {
-                //Append punctuation that may appear before the first token
-                while (currentIndex < token.position[0]) {
+            if (page.content) {
+                let newContent = []
+                newContent.push('\"')
+                let currentIndex = 0
+                page.tokens.forEach((token, indx) => {
+                    //Append punctuation that may appear before the first token
+                    while (currentIndex < token.position[0]) {
+                        newContent.push(page.content[currentIndex])
+                        currentIndex++
+                    }
+                    // Create span containing onClick handler
+                    let tokenSpan = <span key={indx} onClick={() => handleClick(token.value)}>{page.content.slice(token.position[0], token.position[1])}</span>
+                    newContent.push(tokenSpan)
+                    currentIndex = token.position[1]
+                });
+                //Append punctuation that may appear after the last token
+                while (currentIndex <= page.content.length-1) {
                     newContent.push(page.content[currentIndex])
                     currentIndex++
                 }
-                // Create span containing onClick handler
-                let tokenSpan = <span key={indx} onClick={() => handleClick(token.value)}>{page.content.slice(token.position[0], token.position[1])}</span>
-                newContent.push(tokenSpan)
-                currentIndex = token.position[1]
-            });
-            //Append punctuation that may appear after the last token
-            while (currentIndex <= page.content.length-1) {
-                newContent.push(page.content[currentIndex])
-                currentIndex++
+                newContent.push('\"')
+                return (<p>{newContent}</p>)
+            }else{
+                return (<p></p>)
             }
-            newContent.push('\"')
-            return (<p>{newContent}</p>)
         }else{
-            console.log('No page', page);
+            return (<p></p>)
         }
     }
 
@@ -36,10 +40,9 @@ function Page(props){
         //open new page with token
         navigate('/token', {state: {token: token}})
     }
-    if (!props.page || !props.page[0] ) return ''
 
     return (<div id="pageContent">
-        {props.page.content }
+        {transformContent(props.page)}
     </div>)
 }
 
